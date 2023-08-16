347bc3dc1a91bd9178f6a2efdf4dcf361aca4973
Erik <info@terwiel.com>
2023-08-16 06:41:11 +0200

    Put some doc in the Program.cs file

--- CreateFileHeaders2/Program.cs
+++ CreateFileHeaders2/Program.cs
@@ -1,126 +1,160 @@
 ﻿
 using System.Diagnostics;
-using System.Reflection;
-using System.Runtime;
-using System.Text;
 
 namespace CreateFileHeaders
 {
     internal class Program
     {
+        /// <summary>
+        /// Adds a file header to all .cs source files in a VS solution
+        /// </summary>
+        /// <see cref="https://rosettacode.org/wiki/Walk_a_directory/Recursively"/>
 
-        static int wrongFiles = 0;
-        const int MaxActualFileheaderLength = 20;
-        const string Project = "SlmLicenseLib";
-        const string root = "C:\\Users\\erik\\Documents\\Visual Studio 2022\\Projects\\CoCa7a\\SlmLicenseLib";
-        const string copyright = "CopyrightStatement.txt";
-        
+        const int MaxActualFileheaderLength = 40;
+
+        const string root = "C:\\Users\\erik\\Documents\\Visual Studio 2022\\Projects\\CoCa7";
+        // Create two file in each folder that needs its own copyright statement:
+        // CopyrightStatementXAML.txt
+        // CopyrightStatementCs.txt
+        // and exclude these files from the project:        
+        const string copyright = "CopyrightStatement";
+        static string[][] delimiters =
+            {
+                new[] {"#region fileheader", "#endregion fileheader",   "CS",  ".cs"},
+                new[] {"<!-- fileheader"   , " fileheader -->", "XAML",".xaml"}
+            };
+        static List<string> cr = new List<string>();
+        const string HashFile = "ProjectHashCodes";
+        static List<string> HashCodes = new List<string>();
+        static string Project;
+        static int n;
+        static string rem;
         static void Main(string[] args)
         {
-            List<string> cr = new List<string>();
-            if (File.Exists($"{root}\\{copyright}"))
+            foreach (var d in GetProjects(root))
             {
-                Debug.WriteLine($"{root}\\{copyright}");
-                cr = File.ReadAllLines($"{root}\\{copyright}").ToList();
-            }
-            WalkDirectoryTree(new DirectoryInfo(root),cr);
-            Console.WriteLine($"File not processed: {wrongFiles}");
-        }
-
-        static void WalkDirectoryTree(DirectoryInfo root, List<string> _cr)
-        {
-            FileInfo[]? files = null;
-            DirectoryInfo[] subDirs;
-            var dirName = root.FullName;
-            if (dirName.Contains("\\obj") ||
-                dirName.Contains("\\.git") ||
-                dirName.Contains("\\.vs") ||
-                dirName.Contains("\\bin")
-                //root.FullName.EndsWith(".json")
-                //root.FullName.EndsWith(".3dd") ||
-                //root.FullName.EndsWith(".csproj")
-                )
-            {
-                return;
-            }
-
-            
-
-            // First, process all the files directly under this folder
-            try
-            {
-                files = root.GetFiles("*.cs");
-            }
-            // This is thrown if even one of the files requires permissions greater
-            // than the application provides.
-            catch (UnauthorizedAccessException e)
-            {
-                // This code just writes out the message and continues to recurse.
-                // You may decide to do something different here. For example, you
-                // can try to elevate your privileges and access the file again.
-                Console.WriteLine(e.Message);
-                wrongFiles++;
-            }
-            catch (DirectoryNotFoundException e)
-            {
-                Console.WriteLine(e.Message);
-                wrongFiles++;
-            }
-
-            if (files != null)
-            {                
-                foreach (FileInfo fi in files)
-                {
-                    var lines = File.ReadAllLines(fi.FullName).ToList();
-
-                    StripHeader(lines, fi,_cr);
-
-                    // In this example, we only access the existing FileInfo object. If we
-                    // want to open, delete or modify the file, then
-                    // a try-catch block is required here to handle the case
-                    // where the file has been deleted since the call to TraverseTree().
-                    Console.WriteLine(fi.FullName);
-                }
                 
-                // Now find all the subdirectories under this directory.
-                subDirs = root.GetDirectories();
-
-                foreach (DirectoryInfo dirInfo in subDirs)
+                // Print the full path of all .cs files that are somewhere
+                // in the C:\Windows directory or its subdirectories
+                for (n = 0; n < 2; n++)
                 {
-                    //var cr = new List<string>();
-
-                    if (File.Exists($"{dirName}\\{copyright}"))
+                    string hashFile;
+                    HashCodes.Clear();
+                    File.Delete(hashFile =  $"{d.Item2}\\{HashFile}{delimiters[n][2]}.txt");
+                    if (!File.Exists($"{d.Item2}/{copyright}{delimiters[n][2]}.txt"))
                     {
-                        Debug.WriteLine($"{dirName}\\{copyright}");
-                        _cr = File.ReadAllLines($"{dirName}\\{copyright}").ToList();
+                        // do NOT switch CopyrightStatement  files.
+                        // Keep the old one:
+                        continue;
                     }
-
-                    // Resursive call for each subdirectory.
-                    WalkDirectoryTree(dirInfo,_cr);
+                    rem = new[] { "// ", "" }[n];
+                    
+                    foreach (var file in TraverseDirectory(
+                        d.Item2.FullName, f => f.Extension == delimiters[n][3]))
+                    {
+                        Project = d.Item1;
+                        Console.WriteLine(file.FullName);
+                    }
+                    // save the hashes.
+                    File.AppendAllLines(hashFile, HashCodes);
+                    
                 }
             }
-
+            Console.WriteLine("Done.");
+            Console.ReadLine();
         }
 
-        static void StripHeader(List<string> lines, FileInfo fi,List<string> _cr)
+        static IEnumerable<FileInfo> TraverseDirectory(string rootPath, Func<FileInfo, bool> Pattern)
+        {
+            var directoryStack = new Stack<DirectoryInfo>();
+            directoryStack.Push(new DirectoryInfo(rootPath));
+            while (directoryStack.Count > 0)
+            {
+                var dir = directoryStack.Pop();
+                string crFile;
+                if (File.Exists(crFile = $"{dir}\\{copyright}{delimiters[n][2]}.txt"))
+                {                    
+                    cr = File.ReadAllLines(crFile).ToList();
+                }
+
+                try
+                {
+                    foreach (var i in dir.GetDirectories().Where(dr =>
+                     !dr.FullName.Contains("\\bin") &&
+                     !dr.FullName.Contains("\\obj") &&
+                     !dr.FullName.Contains("\\.git") &&
+                     !dr.FullName.Contains("\\.vs")))
+                        directoryStack.Push(i);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue; // We don't have access to this directory, so skip it
+                }
+                foreach (var f in dir.GetFiles().Where(Pattern)) // "Pattern" is a function
+                {
+                    var lines = File.ReadAllLines(f.FullName).ToList();
+                    ReplaceHeader(lines, f);
+
+                    yield return f;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Find all the projects in the solution:
+        /// </summary>
+        /// <param name="root">the root folder of the project</param>
+        /// <returns>The projects in the solution we need to process</returns>
+        static List<(string, DirectoryInfo)> GetProjects(string root)
+        {
+            List<(string, DirectoryInfo)> lst = new List<(string, DirectoryInfo)>();
+            string solution = Directory.GetFiles(root).Where(f => f.EndsWith(".sln")).First();
+            var content = File.ReadAllLines(solution);
+            string[] projs;
+            foreach (string item in content)
+            {
+                if (item.Length >= 7 &&
+                    item[0..7].Equals("Project") &&
+                    (projs = item.Split(new char[] { ',', '=' }))[2].Contains(".csproj")
+                    )
+                {
+                    var proj2 = projs[1][2..(projs[1].Length - 1)];
+                    var proj3 = projs[2][2..(projs[2].Length - 1)];
+                    lst.Add((proj2, new DirectoryInfo(Path.GetFullPath($"{root}\\{proj2}"))));
+                }
+            }
+            return lst;
+        }
+
+        /// <summary>
+        /// Find the old FileHeader region, if any
+        /// </summary>
+        /// <param name="lines">The content of the file that needs a new FileHeader region</param>
+        /// <param name="fi">The file's name</param>
+        static void ReplaceHeader(List<string> lines, FileInfo fi)
         {
             int start = 0;
             int end = 0;
-            int n, i;
+            int m, i;
+            // While we're at it, strip surplus spaces:
             foreach (var l in lines)
             {
                 l.TrimEnd();
             }
-            for (n = 0; n < lines.Count; n++)
+            foreach (var l in lines)
+            {
+                l.Replace("  ", " ");
+            }
+            // find start and end of old region
+            for (m = 0; m < lines.Count; m++)
             {
                 start = end = 0;
-                if (lines[n].ToLower().Contains("#region") &&
-                    lines[n].ToLower().Contains("fileheader"))
+                if (lines[m].ToLower().Contains(delimiters[n][0]))
                 {
-                    start = n;
-                    for (i = n + 1; i < MaxActualFileheaderLength; i++)
+                    start = m;
+                    for (i = m + 1; i < MaxActualFileheaderLength; i++)
                     {
-                        if (lines[i].ToLower().Contains("#endregion"))
+                        if (lines[i].ToLower().Contains(delimiters[n][1]))
                         {
                             end = i;
                             break;
@@ -129,38 +163,45 @@ namespace CreateFileHeaders
                     break;
                 }
             }
-            if (start >= 0 && end > n)
+
+            // remove it:
+            if (start >= 0 && end > start)
             {
                 Console.WriteLine($"{start},{lines[start]}");
                 Console.WriteLine($"{end}, {lines[end]}");
-                lines.RemoveRange(start, end - start + 1);                
+                lines.RemoveRange(start, end - start + 1);
                 while (string.IsNullOrEmpty(lines[0]))
                 {
                     lines.RemoveAt(0);
                 }
-            }
+            }            
+
+            // prepend new FileHeader region:
             List<string> newlines = new List<string>()
             {
-                "#region FileHeader"  ,
-                //"// header v.2",
-                $"// Project: {Project}",
-                $"// Filename:   {fi.Name}",
-                $"// Last write: {fi.LastWriteTime.ToString()}",
-                $"// Creation:   {fi.CreationTime.ToString()}",
-                $"// Code:       {fi.GetHashCode()}/{fi.Length}",
+                delimiters[n][0],
+                $"{rem}Project:     {Project}",
+                $"{rem}Filename:    {fi.Name}",
+                $"{rem}Last write:  {fi.LastWriteTime.ToString()}",
+                $"{rem}Creation:    {fi.CreationTime.ToString()}",               
                 ""
             };
-            foreach(var c in _cr)
+            
+            foreach (var c in cr)
             {
-                
-                newlines.Add($"// {c}");
+                newlines.Add($"{rem}{c}");
             };
-            newlines.Add("#endregion FileHeader");
+            newlines.Add(delimiters[n][1]);
             newlines.Add("");
             newlines.AddRange(lines);
             Debug.WriteLine($"{Project} {fi.Directory.Name} {fi.Name}");
+
+            // disable this line for a trial run:
             File.WriteAllLines(fi.FullName, newlines.ToArray());
-            // Console.ReadLine();
+            // we also gather file hashes and file lengths in a text file 
+            int hash = fi.GetHashCode();
+            long fileLength = fi.Length;
+            HashCodes.Add($"{Project} ; {fi.Name} ; {hash} ; {fileLength}");
 
         }
     }
